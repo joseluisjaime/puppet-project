69c62594d0fe6d9c58933b36ed7f3fbfb2032453
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-12 17:35:00 +0200

    automatic

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+DIR=`pwd`
+cd /home/jose/puppet-project
+git add *
+git commit -m 'automatic'
+git push
 ssh -t root@192.168.122.2 'cd /usr/share/puppet/puppet-project; git pull'
+cd $DIR
