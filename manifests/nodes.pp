7736270c5f3da4b2e0dcdffebc574537c930b64d
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-12 15:33:14 +0200

    t

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3,7 +3,6 @@ include prueba
 }
 node 'client.example.com' inherits ejemplo {
 include httpd
-include prueba2
 }
 node 'client2.example.com' inherits ejemplo {
 include memcached
--- modules/prueba2/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-class prueba2 {
-file { "/root/ficherofunciona":
-owner => root,
-group => root,
-mode => 400,
-source => "puppet:///modules/prueba2/ficherodeprueba",
-}
-}
