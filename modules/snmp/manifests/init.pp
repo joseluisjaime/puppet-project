32d9ab19aefaa1980348658e23dfcc5cfa556b34
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-12 17:23:24 +0200

    t

---
+++ modules/snmp/manifests/init.pp
@@ -0,0 +1,19 @@
+class snmp {
+  
+  package { 'net-snmp':
+    ensure => installed,
+  }
+
+  file {'/etc/snmp/snmp.conf':
+    ensure => file,
+    owner => root,
+    group => root,
+    mode => 0444,
+    source => 'puppet:///modules/snmp/snmpd.conf'
+  }
+
+  service { 'snmpd':
+    ensure => running,
+  }
+
+}
