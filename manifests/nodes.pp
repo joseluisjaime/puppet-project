7690a9ecd5b97abc39cbe8a2ace8cb18e259e47b
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-14 15:24:47 +0200

    automatic

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,8 +2,6 @@ node ejemplo {
 include prueba
 }
 node 'client.example.com' inherits ejemplo {
-include httpd
 }
 node 'client2.example.com' inherits ejemplo {
-include memcached
 }
---
+++ scripts/configura.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nombre=$1
+ip=$2
+
+ssh root@192.168.122.122 "echo $1 > /etc/hostname"
+ssh root@192.168.122.122 "sed -i 's/base/$1/g' /etc/hosts"
+ssh root@192.168.122.122 "echo '192.168.122.2 master.example.com master' >> /etc/hosts"
+ssh root@192.168.122.122 "echo '192.168.122.3 client.example.com client' >> /etc/hosts"
+ssh root@192.168.122.122 "sed -i 's/192.168.122.122/192.168.122.$2/g' /etc/network/interfaces"
+ssh root@192.168.122.122 "reboot"
