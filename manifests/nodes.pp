65df5be4b780bba0247fbff0baa095829b8d5f16
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-15 21:58:31 +0200

    automatic

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,6 +2,9 @@ node ejemplo {
 include prueba
 }
 node 'client.example.com' inherits ejemplo {
+  class { 'snmp':
+    community => 'prueba',
+    }
 }
 node 'client2.example.com' inherits ejemplo {
 }
