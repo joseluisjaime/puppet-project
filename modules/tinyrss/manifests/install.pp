36cc9228cb06d504d0a6a9f9f1f87ebf06d5fef3
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-17 23:50:02 +0200

    automatic

--- modules/tinyrss/manifests/install.pp
+++ modules/tinyrss/manifests/install.pp
@@ -20,7 +20,7 @@ class tinyrss::install {
   exec { "Extract tinyrss":
     subscribe => [ File['tinyrss_directory']],
     path => "/bin:/usr/bin",
-    unless => "find ${tinyrss::tinyrss_directory}",
+#    unless => "find ${tinyrss::tinyrss_directory}",
     command => "tar -xzf /tmp/Tiny-Tiny-RSS-1.8.tar.gz --strip=1 -C ${tinyrss::tinyrss_directory}",
   }
 }
