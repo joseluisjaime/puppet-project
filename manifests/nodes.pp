d4fcf2d2deb72b4b088100a22494ff8fcbd6b384
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-17 16:16:30 +0200

    automatic

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -6,10 +6,10 @@ node 'client.example.com' inherits default {
   class {'mysql':}
 }
 node 'client2.example.com' inherits default {
-#  class {'apache':}
-#  class {'php':}
-#  class {'mysql':}
+  class {'apache':}
+  class {'php':}
+  class {'mysql':}
 
-#  Class['apache'] -> Class['php'] -> Class['mysql']
+  Class['apache'] -> Class['php'] -> Class['mysql']
   
 }
