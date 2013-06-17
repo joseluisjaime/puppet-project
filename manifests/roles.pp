b01d7874c8ef4445b4855565843a7d7bdc0da8a7
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-18 00:52:19 +0200

    automatic

--- manifests/roles.pp
+++ manifests/roles.pp
@@ -1,3 +1,7 @@
+class aptupdate {
+  exec { 'apt-get -y update': }
+}
+
 class role_lamp {
   $role_lamp = hiera('role_lamp')
   
@@ -7,8 +11,13 @@ class role_lamp {
 }
 
 class app_tinyrss {
+  stage { 'update': before => Stage['first'] }
   stage { 'first': before => Stage['main'] }
   stage { 'last': require => Stage['main'] }
+
+  class {'aptupdate':
+    stage => update,
+  }
   
   class {'role_lamp':
     stage => first,
