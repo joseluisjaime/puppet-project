19eddf7a01976c90d30f2f38d1f35610811df5bb
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-17 23:46:14 +0200

    automatic

--- modules/tinyrss/manifests/install.pp
+++ modules/tinyrss/manifests/install.pp
@@ -16,12 +16,12 @@ class tinyrss::install {
     source => 'puppet:///modules/tinyrss/Tiny-Tiny-RSS-1.8.tar.gz',
     path => '/tmp/Tiny-Tiny-RSS-1.8.tar.gz',
   }
-  
+
   exec { "Extract tinyrss":
     subscribe => [ File['tinyrss_directory']],
     refreshonly => true,
     path => "/bin:/usr/bin",
     unless => "find ${tinyrss::tinyrss_directory}",
-    command => "tar -xzf /temp/${tinyrss::tarball} --strip=1 -C ${tinyrss::tinyrss_directory}",
+    command => "tar -xzf /tmp/${tinyrss::tarball} --strip=1 -C ${tinyrss::tinyrss_directory}",
   }
 }
