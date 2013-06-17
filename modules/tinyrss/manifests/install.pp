818119512ad404880312c6dfba6ab1a2e28e9564
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-17 23:32:27 +0200

    automatic

--- modules/tinyrss/manifests/install.pp
+++ modules/tinyrss/manifests/install.pp
@@ -8,11 +8,20 @@ class tinyrss::install {
     path => "${tinyrss::tinyrss_directory}",   
   }
 
+  file {'tinyrss_tarball':
+    ensure => file,
+    owner => www-data,
+    group => www-data,
+    mode => 0775
+    source => 'puppet:///modules/tinyrss/${tinyrss::tarball}',
+    path => '/temp/'
+  }
+  
   exec { "Extract tinyrss":
     #   subscribe => [ File['tinyrss_directory']],
-    #    refreshonly => true,
+    refreshonly => true,
     path => "/bin:/usr/bin",
     unless => "find ${tinyrss::tinyrss_directory}",
-    command => "tar -xzf ${tinyrss::tarball} --strip=1 -C ${tinyrss::tinyrss_directory}",
+    command => "tar -xzf /temp/${tinyrss::tarball} --strip=1 -C ${tinyrss::tinyrss_directory}",
   }
 }
