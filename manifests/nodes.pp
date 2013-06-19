1741d72cd984a1568a3f7025e10c22dd603ef951
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-19 19:51:18 +0200

    automatic

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -10,7 +10,7 @@ node 'client3.example.com' {
 node 'backend2.example.com' {
   include app_tinyrss
 }
-node 'backend2.example.com' {
+node 'backend3.example.com' {
   include apache
   include php
 }
