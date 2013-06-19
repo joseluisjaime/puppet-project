6dac24c4deba769627319e57997aeb5fbf57802d
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-19 12:59:10 +0200

    automatic

--- modules/haproxy/manifests/install.pp
+++ modules/haproxy/manifests/install.pp
@@ -6,13 +6,23 @@ class haproxy::install {
     group => root,
     mode => 0775,
     source => 'puppet:///modules/haproxy/haproxy-1.4.24.tar.gz',
-    path => '/tmp/haproxy-1.4.24.tar.gz',
+    path => '/opt/haproxy-1.4.24.tar.gz',
   }
 
   exec {"Extract haproxy":
     path => "/bin:/usr/bin",
     subscribe => [ File['haproxy_tarball']],
-    unless => "find /tmp/haproxy-1.4.24/doc",
-    command => "tar -xzf /tmp/haproxy-1.4.24.tar.gz",
+    unless => "find /opt/haproxy-1.4.24/doc",
+    command => "tar -xzf /opt/haproxy-1.4.24.tar.gz",
   }
+
+  exec {"install haproxy":
+    path => "/bin:/usr/bin",
+    subscribe => [ Exec['Extract haproxy']],
+    unless => "find /usr/local/sbin/haproxy",
+    cwd => " /opt/haproxy-1.4.24"
+    command => "make TARGET=linux2628 && make install ",
+  }
+
+
 }
