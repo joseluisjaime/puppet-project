7f916838e8963920df2cb125e98d880e1cacf7a0
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-19 10:14:34 +0200

    automatic

--- modules/tinyrss/manifests/config.pp
+++ modules/tinyrss/manifests/config.pp
@@ -11,13 +11,12 @@ class tinyrss::config {
   }
 
   exec {'set owner and group tinyrss':
-    subscribe => [ File['tinyrss config file']],
+    subscribe => [ Exec['Populate tinyrss database']],
     path => "/bin:/usr/bin",
     command => "chown -R www-data:www-data ${tinyrss::tinyrss_directory}"
   }
 
   exec { "Populate tinyrss database":
-    #            subscribe => [ Exec["Extract tinyrss"]],
     path => "/bin:/usr/bin",
     unless => "mysql -u${tinyrss::tinyrss_user} -p${tinyrss::tinyrss_password} ${tinyrss::tinyrss_database} -e \" select * from ttrss_users;\" ",
     command => "mysql -u${tinyrss::tinyrss_user} -p${tinyrss::tinyrss_password} ${tinyrss::tinyrss_database} < ${tinyrss::tinyrss_directory}/schema/ttrss_schema_mysql.sql",
