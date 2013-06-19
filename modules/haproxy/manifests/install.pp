fd00344cb5b4be7883cb874bea8ff1f43bdfbe88
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-19 11:11:27 +0200

    automatic

--- modules/haproxy/manifests/install.pp
+++ modules/haproxy/manifests/install.pp
@@ -8,4 +8,11 @@ class haproxy::install {
     source => 'puppet:///modules/haproxy/haproxy-1.4.24.tar.gz',
     path => '/tmp/haproxy-1.4.24.tar.gz',
   }
+
+  exec {"Extract haproxy":
+    path => "/bin:/usr/bin",
+    subscribe => [ File['haproxy_tarball']],
+    unless => "find /tmp/haproxy-1.4.24/doc",
+    command => "tar -xzf /tmp/haproxy-1.4.24.tar.gz",
+  }
 }
