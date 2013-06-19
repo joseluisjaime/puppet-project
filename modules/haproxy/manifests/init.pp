ab4af4f117a0a64946f8e09278a7caf67a2b6a27
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-19 19:35:55 +0200

    automatic

--- modules/haproxy/manifests/init.pp
+++ modules/haproxy/manifests/init.pp
@@ -3,17 +3,17 @@ class haproxy (
   $haproxy_tarball = 'haproxy-1.4.24.tar.gz',
   $machinename = $hostname,
 
-  $config_server1 = '',
-  $config_name1 = '',
-  $config_ip1 = '',
+  $config_server1 = hiera('config_server1'),
+  $config_name1 = hiera('config_name1'),
+  $config_ip1 = hiera('config_ip1'),
   
-  $config_server2 = '',
-  $config_name2 = '',
-  $config_ip2 = '',
+  $config_server2 = hiera('config_server2'),
+  $config_name2 = hiera('config_name2'),
+  $config_ip2 = hiera('config_ip2'),
 
-  $config_server3 = '',
-  $config_name3 = '',
-  $config_ip3 = '',
+  $config_server3 = hiera('config_server3'),
+  $config_name3 = hiera('config_name3'),
+  $config_ip3 = hiera('config_ip3'),
 
   
 )  {
