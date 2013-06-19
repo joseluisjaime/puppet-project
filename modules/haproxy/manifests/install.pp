014581c637f918ef95478346f74e6351e30ed631
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-19 11:00:47 +0200

    automatic

--- modules/haproxy/manifests/install.pp
+++ modules/haproxy/manifests/install.pp
@@ -5,7 +5,7 @@ class haproxy::install {
     owner => root,
     group => root,
     mode => 0775,
-    source => 'puppet:///modules/haproxy/${haproxy::haproxy_tarball}',
-    path => '/tmp/${haproxy::haproxy_tarball}',
+    source => 'puppet:///modules/haproxy/haproxy-1.4.24.tar.gz',
+    path => '/tmp/haproxy-1.4.24.tar.gz',
   }
 }
