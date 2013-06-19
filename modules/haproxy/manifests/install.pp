0ac545428b128616ef538ceb7dedba1ab6a0725b
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-19 13:35:21 +0200

    automatic

--- modules/haproxy/manifests/install.pp
+++ modules/haproxy/manifests/install.pp
@@ -1,6 +1,6 @@
 class haproxy::install {
 
-  file['haproxy_tarball'] -> Exec["Extract haproxy"] -> Exec["install haproxy make"] -> Exec["install haproxy make install"]
+#  file['haproxy_tarball'] -> Exec["Extract haproxy"] -> Exec["install haproxy make"] -> Exec["install haproxy make install"]
   
   
   file {'haproxy_tarball':
@@ -13,26 +13,26 @@ class haproxy::install {
 
   exec {"Extract haproxy":
     path => "/bin:/usr/bin",
-#    subscribe => [ File['haproxy_tarball']],
+    subscribe => [ File['haproxy_tarball']],
     unless => "find /opt/haproxy-1.4.24/doc",
     command => "tar -xzf /opt/haproxy-1.4.24.tar.gz",
   }
 
-  exec {"install haproxy make":
-    path => "/bin:/usr/bin",
-    subscribe => [ Exec['Extract haproxy']],
-    unless => "find /usr/local/sbin/haproxy",
+#  exec {"install haproxy make":
+#    path => "/bin:/usr/bin",
+#    subscribe => [ Exec['Extract haproxy']],
+#    unless => "find /usr/local/sbin/haproxy",
    # cwd => " /opt/haproxy-1.4.24/",
-    command => "make TARGET=linux2628 -C /opt/haproxy-1.4.24",
-  }
+#    command => "make TARGET=linux2628 -C /opt/haproxy-1.4.24",
+#  }
 
-  exec {"install haproxy make install":
-    path => "/bin:/usr/bin",
-    subscribe => [ Exec['install haproxy make']],
+#  exec {"install haproxy make install":
+#    path => "/bin:/usr/bin",
+#    subscribe => [ Exec['install haproxy make']],
    # cwd => " /opt/haproxy-1.4.24",
-    unless => "find /usr/local/sbin/haproxy",
-    command => "make install -C /opt/haproxy-1.4.24",
-  }
+#    unless => "find /usr/local/sbin/haproxy",
+#    command => "make install -C /opt/haproxy-1.4.24",
+#  }
 
 
 }
