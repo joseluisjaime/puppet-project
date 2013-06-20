1d7bfbaf2a243edc260e19febf80c61293b54e41
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-20 10:17:51 +0200

    automatic

--- scripts/vmtool.sh
+++ scripts/vmtool.sh
@@ -31,6 +31,7 @@ sleep 15
 ssh root@192.168.122.122 "echo $NAME > /etc/hostname"
 ssh root@192.168.122.122 "sed -i 's/base/$NAME/g' /etc/hosts"
 ssh root@192.168.122.122 "echo '192.168.122.2 master.example.com master' >> /etc/hosts"
+ssh root@192.168.122.122 "echo 'report=true' >> /etc/puppet/puppet.conf"
 ssh root@192.168.122.122 "sed -i 's/192.168.122.122/192.168.122.$IP/g' /etc/network/interfaces"
 ssh root@192.168.122.122 "sed -i 's/START=no/START=yes/g' /etc/default/puppet"
 ssh root@192.168.122.122 "reboot"
