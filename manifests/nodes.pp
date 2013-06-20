8d56d1a22e8cd3a319f7a0b82ce2914a18f949ba
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-20 02:24:49 +0200

    automatic

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -17,3 +17,15 @@ node 'backend3.example.com' {
 node 'puppetclient1.example.com' {
   include prueba
 }
+node 'balanaux.example.com' {
+  include apache
+  include php
+}
+node 'balanaux.example.com' {
+  include apache
+  include php
+}
+node 'balanaux.example.com' {
+  include apache
+  include php
+}
