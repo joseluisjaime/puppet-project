fdcbbdc28d3b743ecb66063163575fcaf23c0f93
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-20 23:11:15 +0200

    automatic

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -7,15 +7,6 @@ node 'client2.example.com' {
 node 'client3.example.com' {
 
 }
-node 'backend2.example.com' {
-
-}
-node 'backend3.example.com' {
-
-}
-node 'puppetclient1.example.com' {
-
-}
 node 'prueba1.example.com' {
   include app_tinyrss
 }
