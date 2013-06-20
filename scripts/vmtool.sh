f459f508ed73158599c1612bc75c4633eb35f05d
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-20 10:49:47 +0200

    automatic

--- scripts/vmtool.sh
+++ scripts/vmtool.sh
@@ -27,7 +27,7 @@ fi
 
 virt-clone --connect qemu:///system --original base --name $NAME --file /media/linux/imagen/$NAME.img
 virsh start $NAME
-sleep 15
+sleep 16
 ssh root@192.168.122.122 "echo $NAME > /etc/hostname"
 ssh root@192.168.122.122 "sed -i 's/base/$NAME/g' /etc/hosts"
 ssh root@192.168.122.122 "echo '192.168.122.2 master.example.com master' >> /etc/hosts"
@@ -35,7 +35,7 @@ ssh root@192.168.122.122 "echo 'report=true' >> /etc/puppet/puppet.conf"
 ssh root@192.168.122.122 "sed -i 's/192.168.122.122/192.168.122.$IP/g' /etc/network/interfaces"
 ssh root@192.168.122.122 "sed -i 's/START=no/START=yes/g' /etc/default/puppet"
 ssh root@192.168.122.122 "reboot"
-sleep 20
+sleep 23
 ssh root@192.168.122.2 "puppet cert --sign $NAME.example.com"
 sleep 2
 ssh root@192.168.122.$IP "puppet agent -t"
