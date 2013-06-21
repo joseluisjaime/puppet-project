b7a1240adc56b1254e6d2c70ee2a3da252dc1f45
Jose Luis <joseluis.jaime.g@gmail.com>
2013-06-21 04:11:22 +0200

    automatic

---
+++ modules/apache_simple/manifests/init.pp
@@ -0,0 +1,23 @@
+class apache_simple {
+  Package['apache package']  -> File['index.html'] ~> Service['apache service']
+
+  package { 'apache package':
+    ensure => installed,
+    name => "apache2",
+  }
+
+  file {'index.html file':
+    ensure => file,
+    owner => www-data,
+    group => www-data,
+    mode => 0640,
+    source => "puppet:///modules/apache/index.html",
+    path => "/vaw/www/index.html",
+  }
+
+  service {'apache service':
+    ensure => running,
+    name => "apache2",
+  }
+}
+        
